e18b4cc62e6ad9706da1bc6d1e02189e4cda6e9c
ardillaCHIKI <svegazos@student.universidadunie.com>
2025-10-05 08:33:52 +0000

    cambios para ver si funciona ;-;

--- mnist_trainer.py
+++ mnist_trainer.py
@@ -1,43 +1,25 @@
+import tensorflow as tf
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
 from tensorflow.keras.utils import to_categorical
-import matplotlib.pyplot as plt
 
-# Cargar datos
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 x_train = x_train.reshape(-1, 28, 28, 1) / 255.0
 x_test = x_test.reshape(-1, 28, 28, 1) / 255.0
 y_train = to_categorical(y_train)
 y_test = to_categorical(y_test)
 
-# Definir modelo CNN
 model = Sequential([
-    Conv2D(32, (3,3), activation='relu', input_shape=(28,28,1)),
-    MaxPooling2D((2,2)),
+    Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(28, 28, 1)),
+    MaxPooling2D(pool_size=(2, 2)),
     Flatten(),
     Dense(128, activation='relu'),
     Dense(10, activation='softmax')
 ])
 
-# Compilar y entrenar
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-history = model.fit(x_train, y_train, epochs=5, validation_data=(x_test, y_test))
+model.fit(x_train, y_train, epochs=5, validation_data=(x_test, y_test))
 
-# Guardar modelo
-model.save('mnist_model.keras')
-
-# Graficar precisión
-plt.plot(history.history['accuracy'], label='Entrenamiento')
-plt.plot(history.history['val_accuracy'], label='Validación')
-plt.title('Precisión')
-plt.legend()
-plt.savefig('static/accuracy.png')
-plt.clf()
-
-# Graficar pérdida
-plt.plot(history.history['loss'], label='Entrenamiento')
-plt.plot(history.history['val_loss'], label='Validación')
-plt.title('Pérdida')
-plt.legend()
-plt.savefig('static/loss.png')
+# Guardar solo el modelo final
+model.save("modelo_final.h5")
